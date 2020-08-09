4f1584d7f8ce3e453d19b5bab3a28723c2254245
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-09 21:56:01 +0300

    Updated-5 (додав Required).

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Automobile_repair_shop.Models
 {
     public class Order
     {
         public int OrderId { get; set; }
+        [Required(ErrorMessage = "Введите имя!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Введите хоть адрес 1!")]
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
+        [Required(ErrorMessage = "Введите город для получения!")]
         public string City { get; set; }
         public bool GiftWrap { get; set; }
         public bool Dispatched { get; set; }
